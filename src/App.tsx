0a83f79555e6fd52a80d9424f4c9149a42dba4e6
草をこして森 <kyou0317kyou0317@gmail.com>
2024-09-29 13:45:33 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import CounterBlock from "./components/CounterBlock";
 import DisplayNumber from "./components/display_number";
-//import "./App.css"; // CSSファイルをインポート
+import "./styles/App.css";
 
 const App: React.FC = () => {
   const [blockNumber, setBlockNumber] = useState(3);
   return (
-    <div className="container">
-      <div>
+    <div className="App_container">
+      <div className="App_sentence_container">
         <h1>CounterMemo(カウンターメモ)</h1>
         <p>カウンターとメモが使えるよ</p>
         <DisplayNumber
